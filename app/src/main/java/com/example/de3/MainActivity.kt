51622e56805ad8fad368df8ab6e0d07b10567eb2
SergeyL-club <damnazzel@gmail.com>
2022-04-04 12:10:08 +0500

    stage 0

---
+++ app/src/main/java/com/example/de3/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.de3
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.auth_form)
+
+    }
+}
